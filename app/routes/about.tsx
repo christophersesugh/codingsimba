1667435cc2a4785b31683e5b27fc2f96364bb459
christophersesugh <christohybrid185@gmail.com>
2024-01-05 14:09:39 +0100

    core: add shadcn

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -3,7 +3,6 @@ import { Section } from "~/components/section";
 import { DiscordButton } from "~/components/discord-button";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-// import { mdxBundle } from "~/utils/bundler.server";
 import { Markdown } from "~/components/markdown";
 import { metaFn } from "~/utils/meta";
 import { aboutContent } from "~/constants/page-content.server";
@@ -11,7 +10,6 @@ import { aboutContent } from "~/constants/page-content.server";
 export const meta: MetaFunction = metaFn;
 
 export async function loader() {
-  // const { code: content } = await mdxBundle({ source: aboutContent });
   return json({ content: aboutContent });
 }
 
